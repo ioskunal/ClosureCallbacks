f9d547df1d33fb537467d854ea1625fff9d6eda5
Kunal Gupta <ioskunal@gmail.com>
2020-03-03 20:00:43 +0530

    Scene Delegate removed.

---
+++ ClosureCallbacks/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  ClosureCallbacks
+//
+//  Created by Kunal Gupta on 28/02/20.
+//  Copyright © 2020 Kunal Gupta. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
